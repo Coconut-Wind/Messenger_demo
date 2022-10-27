a03c0ca7480a3f4bb18ce2e389df8c77ef61392f
chordfish <592229466@qq.com>
2022-10-27 09:17:46 +0800

    修复 导出后image变紫的问题

--- Assets/Scripts/UI/StartButton.cs
+++ Assets/Scripts/UI/StartButton.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class StartButton : MonoBehaviour
 {
 
     public void OnStartPressed()
     {
         SceneManager.LoadScene(2);
+    }
+
+    public void OnExitPressed()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();  
+#endif
     }
 }
